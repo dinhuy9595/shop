0ad0b9b0aec3233e2d1a7d025c2daea5464670f6
yuu <dinhuy9595@gmail.com>
2021-10-30 03:22:52 +0700

    add product model

---
+++ backend/src/domain/model/product.go
@@ -0,0 +1,14 @@
+package model
+
+type Product truct {
+	ID 	uint
+	Name	string
+	Price	float32
+	Urls	string
+	Images	string
+
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
